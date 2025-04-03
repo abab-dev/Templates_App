9095d33af534de5e83a1be66390368a916197119
abab-dev <developement.ab@gmail.com>
2025-04-03 19:56:13 +0530

    feat: Improve ViewHtmlDialog for better code display

--- components/ui/custom/ViewHtmlDialog.jsx
+++ components/ui/custom/ViewHtmlDialog.jsx
@@ -8,13 +8,13 @@ import {
 
 export default function ViewCodeDialog({ openDialog, htmlCode, closeDialog }) {
   return (
-    <Dialog open={openDialog}>
+    <Dialog open={openDialog} onOpenChange={closeDialog}>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Code</DialogTitle>
           <DialogDescription asChild >
-            <div className="max-h-[400px] overflow-auto bg-gray-200 rounded-lg p-5">
-              <pre>
+            <div className="max-h-[400px] overflow-x-auto bg-gray-200 rounded-lg p-5">
+              <pre className="whitespace-pre-wrap break-all ">
                 <code>
                   {htmlCode}
                 </code>
