3eaa23fcce531b2543a5af2e4ad2d49f2176e277
abab-dev <developement.ab@gmail.com>
2025-04-03 19:57:33 +0530

    feat: Add visual feedback when code is copied to clipboard

--- components/ui/custom/ViewHtmlDialog.jsx
+++ components/ui/custom/ViewHtmlDialog.jsx
@@ -6,11 +6,17 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
+import { useState } from "react";
 
 export default function ViewCodeDialog({ openDialog, htmlCode, closeDialog }) {
+  const [copied, setCopied] = useState(false);
 
-  const handleCopyClick = () => {
-    navigator.clipboard.writeText(htmlCode);
+  const handleCopyClick = async () => {
+    await navigator.clipboard.writeText(htmlCode);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
   };
 
   return (
@@ -18,7 +24,7 @@ export default function ViewCodeDialog({ openDialog, htmlCode, closeDialog }) {
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Code</DialogTitle>
-          <Button size="sm" onClick={handleCopyClick}>Copy</Button>
+          <Button size="sm" onClick={handleCopyClick}>{copied ? "Copied!" : "Copy"}</Button>
           <DialogDescription asChild >
             <div className="max-h-[400px] overflow-x-auto bg-gray-200 rounded-lg p-5">
               <pre className="whitespace-pre-wrap break-all ">
