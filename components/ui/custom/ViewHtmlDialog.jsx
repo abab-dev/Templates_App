6d5e732f7dee9c7fc8c3847fa5cd1431f6680f20
abab-dev <developement.ab@gmail.com>
2025-04-03 19:56:14 +0530

    feat: Add copy button to ViewCodeDialog to copy code to clipboard

--- components/ui/custom/ViewHtmlDialog.jsx
+++ components/ui/custom/ViewHtmlDialog.jsx
@@ -5,13 +5,20 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button"
 
 export default function ViewCodeDialog({ openDialog, htmlCode, closeDialog }) {
+
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(htmlCode);
+  };
+
   return (
     <Dialog open={openDialog} onOpenChange={closeDialog}>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Code</DialogTitle>
+          <Button size="sm" onClick={handleCopyClick}>Copy</Button>
           <DialogDescription asChild >
             <div className="max-h-[400px] overflow-x-auto bg-gray-200 rounded-lg p-5">
               <pre className="whitespace-pre-wrap break-all ">
