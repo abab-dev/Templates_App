216f7ea2475162a2702f8aca4335cda652dc30ef
abab-dev <developement.ab@gmail.com>
2025-04-04 15:50:36 +0530

    save template partial working

--- components/ui/custom/EditorHeader.jsx
+++ components/ui/custom/EditorHeader.jsx
@@ -3,9 +3,21 @@ import React from 'react'
 import Image from 'next/image'
 import { Button } from '../button'
 import { Code, Monitor, Smartphone } from 'lucide-react'
-import { useScreenSize } from '@/app/provider'
+import { useEmailTemplate, useScreenSize } from '@/app/provider'
+import { useMutation } from 'convex/react'
+import { api } from '@/convex/_generated/api'
+import { useParams } from 'next/navigation'
 function EditorHeader({ viewHTMLCode }) {
   const { screenSize, setScreenSize } = useScreenSize()
+  const {templateId} = useParams()
+  const updateEmailTemplate = useMutation(api.emailTemplate.UpdateTemplateDesign)
+  const {emailTemplate,setEmailTemplate} = useEmailTemplate()
+  const onSaveTemplate = async () =>{
+    await updateEmailTemplate({
+      tId:templateId,
+      design:emailTemplate,
+    })
+  }
   return (
     <div className='flex shadow-sm justify-between items-center'>
       <Image src="/logo.png" alt='logo' width={50} height={80}></Image>
@@ -15,7 +27,8 @@ function EditorHeader({ viewHTMLCode }) {
         <Button variant="ghost" onClick={() => setScreenSize('mobile')} className={`${screenSize == 'mobile' && 'bg-purple-100 text-primary'}`}><Smartphone />Mobile</Button>
 
       </div >
-      <div className="flex gap-3 "><Button>save template</Button>
+      <div className="flex gap-3 "><Button
+      onClick={onSaveTemplate}>save template</Button>
         <Button variant={'outline'}>send test email </Button>
 
         <Button className="hover:text-primary hover:bg-purple-100" variant={'ghost'}
