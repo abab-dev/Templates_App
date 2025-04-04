713871c3bce2d949c75030a9b66f18287d89dd73
abab-dev <developement.ab@gmail.com>
2025-04-04 09:41:12 +0530

    dashboard design start

---
+++ app/(main)/dashboard/layout.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "@/components/ui/custom/Header";
+export default function DashboardLayout({ children }) {
+  <div>
+    <Header />
+    {children}
+
+  </div>
+}
--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -13,7 +13,7 @@ function Dashboard() {
 
   return (
     <div>
-      <Header />
+      {/* <Header /> */}
       <div className="p-10 md:px-28 lg:px-40 xl:px-56 mt-16">
         <div className="flex justify-between items-center">
           <h2 className="font-bold text-3xl">{!user ? <div>Please sign in.</div> : <div>Hello {user.id}!</div>}</h2>
