ca358bc1723745c59384652ca51ef68d2f53c36d
abab-dev <developement.ab@gmail.com>
2025-04-04 11:30:30 +0530

    feat: Display user email instead of ID on dashboard

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -16,7 +16,7 @@ function Dashboard() {
       {/* <Header /> */}
       <div className="p-10 md:px-28 lg:px-40 xl:px-56 mt-16">
         <div className="flex justify-between items-center">
-          <h2 className="font-bold text-3xl">{!user ? <div>Please sign in.</div> : <div>Hello {user.id}!</div>}</h2>
+          <h2 className="font-bold text-3xl">{!user ? <div>Please sign in.</div> : <div>Hello {user?.primaryEmailAddress?.emailAddress} !</div>}</h2>
           <Button>Create New Email Template</Button>
         </div>
         <EmailTemplateList></EmailTemplateList>
