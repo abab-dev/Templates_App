34b17f3f619a281aa3a76195fc52c2720ae0f0ad
abab-dev <developement.ab@gmail.com>
2025-04-04 23:29:52 +0530

    fix: Change default tab value to "AI" in create page

--- app/(main)/dashboard/create/page.jsx
+++ app/(main)/dashboard/create/page.jsx
@@ -23,7 +23,7 @@ export default function CreateNew() {
       <div className="flex flex-col items-center">
         <h2 className="font-bold text-3xl">CREATE NEW EMAIL TEMPLATE</h2>
         <p className="text-lg text-gray-400"> Professional Email Templates at Your Fingertips</p>
-        <Tabs defaultValue="account" className="w-[500px] mt-10">
+        <Tabs defaultValue="AI" className="w-[500px] mt-10">
           <TabsList>
             <TabsTrigger value="AI">Create with AI <Sparkles /></TabsTrigger>
             <TabsTrigger value="SCRATCH">Start from Scratch </TabsTrigger>
