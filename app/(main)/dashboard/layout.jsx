53c1b22d1d4facc826963abd025f3ae6366b93ca
abab-dev <developement.ab@gmail.com>
2025-04-04 23:57:24 +0530

    cleanup header for dashboard and homepage

--- app/(main)/dashboard/layout.jsx
+++ app/(main)/dashboard/layout.jsx
@@ -4,7 +4,7 @@ export default function DashboardLayout({ children }) {
   return (
     // Apply gradient and min-height to the layout container
     <div className="min-h-screen bg-gradient-to-br from-purple-200 via-pink-200 to-red-100">
-      <Header />
+      <Header isDashboard={false} />
       {children}
     </div>
   );
